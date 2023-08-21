575c8c4cd0a3c39d2b4e8a850d2f0e18fe3d39f8
Dread <34528298+islandbitcoin@users.noreply.github.com>
2023-08-20 20:21:31 -0400

    auto update script

--- reset-all.sh
+++ reset-all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-kubectl delete deployments --all && \
-kubectl delete rs --all && \
-kubectl delete pods --all && \
-kubectl delete svc --all && \
-kubectl delete daemonsets --all && \
-kubectl delete statefulsets --all && \
-kubectl delete jobs --all && \
-kubectl delete cronjobs --all && \
-kubectl delete configmaps --all && \
-kubectl delete secrets --all && \
+kubectl delete deployments "$1" && \
+kubectl delete rs "$1" && \
+kubectl delete pods "$1" && \
+kubectl delete svc "$1" && \
+kubectl delete daemonsets "$1" && \
+kubectl delete statefulsets "$1" && \
+kubectl delete jobs "$1" && \
+kubectl delete cronjobs "$1" && \
+kubectl delete configmaps "$1" && \
+kubectl delete secrets "$1" && \
 echo "All resources deleted" 
