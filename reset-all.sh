42c9b0682e81d31d2a90731a3507b678fd7c2f64
Dread <34528298+islandbitcoin@users.noreply.github.com>
2023-08-21 21:01:46 -0400

    auto update script

--- reset-all.sh
+++ reset-all.sh
@@ -10,4 +10,10 @@ kubectl delete jobs --all && \
 kubectl delete cronjobs --all && \
 kubectl delete configmaps --all && \
 kubectl delete secrets --all && \
+kubectl delete kafka --all && \
+kubectl delete KafkaConnect --all && \
+kubectl delete crd --all && \
+kubectl delete ingressclass --all && \
+kubectl delete ingress --all && \
+kubectl delete pvc --all && \
 echo "All resources deleted" 
