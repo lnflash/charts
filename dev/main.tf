f0e34f3a2a0fcc84d3874a9756f3c2e29ab8e8cb
Dread <34528298+islandbitcoin@users.noreply.github.com>
2023-08-29 15:19:55 -0400

    auto update script

--- dev/main.tf
+++ dev/main.tf
@@ -107,3 +107,9 @@ provider "kubernetes" {
     manifest_resource = true
   }
 }
+
+provider "kubernetes-alpha" {
+  host                   = "https://${data.google_container_cluster.primary.private_cluster_config.0.private_endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
